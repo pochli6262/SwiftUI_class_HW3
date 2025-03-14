ea84cca26fd6eb5de3e7e83afa126bd91e886604
jason <pochli6262@gmail.com>
2025-03-14 09:47:09 +0800

    Initial Commit

---
+++ SwiftUI_class_HW3/SwiftUI_class_HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_class_HW3App.swift
+//  SwiftUI_class_HW3
+//
+//  Created by 廖柏鈞 on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_class_HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
